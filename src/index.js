a0278bcd5a036b71c1e14b6d6a281b4aba0bc973
Akhilesh Prasanna <37016273+bruchimp3@users.noreply.github.com>
2023-11-14 13:04:07 +0530

    Added structure and CSS

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
+
+const Booklist = () =>{
+return(
+  <section className='booklist'>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    
+  </section>
+);
+}
+
+const Book = () => {
+return(
+  <article className='book'>
+    <Image/>
+    <Title/>
+    <Author/>
+  </article>
+);
+}
+
+const Image = () => (<img src="https://images-eu.ssl-images-amazon.com/images/I/715qi-cIbML._AC_UL600_SR600,400_.jpg" alt="Don't Believe Everything You Think"/>)
+const Title = () => <h2>Don't Believe Everything You Think</h2>
+const Author = () => <h3>Joseph Nguyen</h3>
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<Booklist/>);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
